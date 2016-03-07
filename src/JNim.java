732dde2465db3146d90923080e898380934d27d8
matteojoliveau <matteojoliveau@outlook.com>
2016-03-07 19:37:44 +0100

    Added PvP mode

--- src/JNim.java
+++ src/JNim.java
@@ -4,12 +4,12 @@ public class JNim
 {
 	public static void main(String[] args)
 	{
-		String ver = "0.3.1";
+		String ver = "0.3.5";
 		Scanner in = new Scanner(System.in);
 		
         System.out.println("\n--------------------------------------------");
 		System.out.println("\nJNim " + ver + " by Matteo Joliveau ©2016");
-		System.out.println("\nChoose the GameMode:\n0) Read the rules [COMING SOON] \n1) IA vs IA easy \n2) Player vs IA easy \n15)Exit ");
+		System.out.println("\nChoose the GameMode:\n0) Read the rules [COMING SOON] \n1) IA vs IA easy \n2) Player vs IA easy \n3) Player vs Player \n15)Exit ");
 		int choice = in.nextInt();
 		
 		switch(choice)
@@ -34,6 +34,12 @@ public class JNim
                 break;
             }
             
+            case(3):
+            {
+                pvp();
+                main(null);
+                break;
+            }
             case(15):
             {
                 System.exit(0);
@@ -83,6 +89,45 @@ public class JNim
         String nome = in.next();
         NimHumanPlayer p1 = new NimHumanPlayer(nome);
         NimNonExpertPlayer p2 = new NimNonExpertPlayer("Albert");
+        
+        NimGame game = new NimGame(p1, p2);
+        game.setPlayer1(p1);
+        game.setPlayer2(p2);
+        int gameState = 0;
+        
+        while(!game.isGameOver())
+        {
+            System.out.println(game.toString());
+            gameState = game.executeMove();
+            if(gameState == 101 || gameState == 102)
+                break;
+            else    
+                System.out.println("Removed " + gameState + " marbles");
+                
+            try
+            {
+                Thread.sleep(1000);    
+            } catch(InterruptedException ex)
+              {
+                Thread.currentThread().interrupt();  
+              }
+        }
+        if(game.getNextPlayer() == p1)
+            System.out.println("\n" + p1.toString() + " won the game!");
+		else
+			System.out.println("\n" + p2.toString() + " won the game!");
+    }
+    
+    public static void pvp()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("\nInsert your UserName ");
+        System.out.print("\nPlayer 1: ");
+        String nameP1 = in.next();
+        System.out.print("\nPlayer 2: ");
+        String nameP2 = in.next();
+        NimHumanPlayer p1 = new NimHumanPlayer(nameP1);
+        NimHumanPlayer p2 = new NimHumanPlayer(nameP2);
         
         NimGame game = new NimGame(p1, p2);
         game.setPlayer1(p1);
